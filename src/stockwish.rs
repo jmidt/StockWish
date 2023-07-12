e7653cb6882a24e69715299d5212f2bcf5296e45
Jonatan Midtgaard <jonatan.midtgaard@gmail.com>
2023-07-12 14:27:45 +0200

    Added alpha-beta pruning to minmax algorithm

--- src/stockwish.rs
+++ src/stockwish.rs
@@ -8,12 +8,12 @@ use chess::ChessMove;
 use chess::Game;
 use chess::MoveGen;
 
-struct Performance {
+struct Statistics {
     start: Instant,
     iterations: i32,
 }
 
-impl Performance {
+impl Statistics {
     pub fn new() -> Self {
         Self {
             start: Instant::now(),
@@ -48,17 +48,25 @@ impl StockWish {
         let board = game.current_position();
         let moves = MoveGen::new_legal(&board);
         const DEPTH: i32 = 3;
+        let mut stats = Statistics::new();
 
-        let mut perf = Performance::new();
-        let best_move = match game.side_to_move() {
-            chess::Color::Black => {
-                moves.min_by_key(|&m| negamax(&board.make_move_new(m), &mut perf, DEPTH))
-            }
-            chess::Color::White => {
-                moves.max_by_key(|&m| negamax(&board.make_move_new(m), &mut perf, DEPTH))
-            }
+        let mut algorithm = |m: ChessMove| {
+            negamax_alpha_beta(
+                &board.make_move_new(m),
+                &mut stats,
+                DEPTH,
+                i32::MIN,
+                i32::MAX,
+            )
         };
-        perf.stop();
+        // let mut algorithm = |m: ChessMove| negamax(&board.make_move_new(m), &mut stats, DEPTH);
+
+        // Get the move that leads to the best scoring child board.
+        let best_move = match game.side_to_move() {
+            chess::Color::Black => moves.min_by_key(|&m| algorithm(m)),
+            chess::Color::White => moves.max_by_key(|&m| algorithm(m)),
+        };
+        stats.stop();
         return best_move;
     }
 }
@@ -66,16 +74,16 @@ impl StockWish {
 //
 // Return a score for a board state, using a recursive negamax strategy
 //
-fn negamax(board: &Board, perf: &mut Performance, remaining_depth: i32) -> i32 {
+fn negamax(board: &Board, stats: &mut Statistics, remaining_depth: i32) -> i32 {
     if remaining_depth == 0 {
         // This is a leaf node, so we evaluate
-        perf.increment();
+        stats.increment();
         evaluate_board(board)
     } else {
         // Evaluate children and take either min or max, depending on whose turn it is
         let child_scores = MoveGen::new_legal(board)
             .map(|m| board.make_move_new(m))
-            .map(|b| negamax(&b, perf, remaining_depth - 1));
+            .map(|b| negamax(&b, stats, remaining_depth - 1));
         // There may not be any valid moves, such as in the case of a checkmate. It should not happen otherwise.
         if child_scores.len() == 0 {
             return evaluate_board(board);
@@ -84,6 +92,61 @@ fn negamax(board: &Board, perf: &mut Performance, remaining_depth: i32) -> i32 {
             chess::Color::Black => child_scores.min().unwrap(),
             chess::Color::White => child_scores.max().unwrap(),
         }
+    }
+}
+
+//
+// Return a score for a board state, using a recursive negamax strategy, with alpha-beta pruning.
+// To enable pruning, we must evaluate the board score for all nodes, not just leaf nodes. This
+// costs us a few board evaluations, but the pruning makes it worth it.
+//
+fn negamax_alpha_beta(
+    board: &Board,
+    stats: &mut Statistics,
+    remaining_depth: i32,
+    _alpha: i32,
+    _beta: i32,
+) -> i32 {
+    if remaining_depth == 0 {
+        // This is a leaf node, so we evaluate
+        stats.increment();
+        evaluate_board(board)
+    } else {
+        let mut alpha = _alpha;
+        let mut beta = _beta;
+        // Evaluate children and take either min or max, depending on whose turn it is
+        let child_boards = MoveGen::new_legal(board).map(|m| board.make_move_new(m));
+        // There may not be any valid moves, such as in the case of a checkmate. It should not happen otherwise.
+        if child_boards.len() == 0 {
+            return evaluate_board(board);
+        }
+        let mut best_value = match board.side_to_move() {
+            chess::Color::White => i32::MIN,
+            chess::Color::Black => i32::MAX,
+        };
+        for child_board in child_boards {
+            let child_score =
+                negamax_alpha_beta(&child_board, stats, remaining_depth - 1, alpha, beta);
+            match board.side_to_move() {
+                // Maximizing player
+                chess::Color::White => {
+                    best_value = std::cmp::max(best_value, child_score);
+                    if beta < best_value {
+                        break;
+                    }
+                    alpha = std::cmp::max(alpha, best_value);
+                }
+                // Minimizing player
+                chess::Color::Black => {
+                    best_value = std::cmp::min(best_value, child_score);
+                    if best_value < alpha {
+                        break;
+                    }
+                    beta = std::cmp::min(beta, best_value);
+                }
+            }
+        }
+        return best_value;
     }
 }
 
